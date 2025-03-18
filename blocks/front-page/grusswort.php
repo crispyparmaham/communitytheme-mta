2e5a59aff0a10389fae074818939a089fd62a397
Matthias Marx <matthias.marx@smail.th-koeln.de>
2025-03-18 16:35:00 +0100

    removed portrait and grusswort from frontpage if they don not exist

--- blocks/front-page/grusswort.php
+++ blocks/front-page/grusswort.php
@@ -3,14 +3,14 @@
 $portrait = get_field('portrait', 'option'); 
 $gruswort = get_field('gruswort', 'option'); 
 ?>
-
+<?php if($portrait || $gruswort) : ?>
 <div class="gruswort-container inner-max-width">
     <!-- Portrait-Bild -->
     <div class="portrait">
         <?php if ($portrait) : ?>
             <img src="<?php echo esc_url($portrait['url']); ?>" alt="<?php echo esc_attr($portrait['alt']); ?>">
         <?php else : ?>
-            <img src="https://via.placeholder.com/150" alt="Platzhalter-Portrait">
+            <!-- <img src="https://via.placeholder.com/150" alt="Platzhalter-Portrait"> -->
         <?php endif; ?>
     </div>
 
@@ -18,8 +18,7 @@ $gruswort = get_field('gruswort', 'option');
     <div class="content">
         <?php if ($gruswort) : ?>
             <p><?php echo wp_kses_post($gruswort); ?></p>
-        <?php else : ?>
-            <p>Kein Grußwort verfügbar.</p>
         <?php endif; ?>
     </div>
 </div>
+<?php endif; ?>
